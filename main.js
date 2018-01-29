1217ee528644fa6d958514e60db72b9ff1d77401
Jan Tiedemann <jantdm@gmail.com>
2018-01-29 19:29:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function()
+{
+	init();
+});
+
+
+function init()
+{
+	$('.pack').click(toggle);
+	$('.more').click(toggle);
+}
+
+function toggle()
+{
+	var pack = $(this);
+	var art = pack.parent();
+	var more = art.find('.more');
+	var div = art.find('#description');
+	
+		
+	if (div.is(':hidden')) 
+	{
+		div.slideDown('slow');
+		more.text('hide details');
+	}
+	else
+	{
+		div.slideUp('slow');
+		more.text('show details');
+	}
+}
